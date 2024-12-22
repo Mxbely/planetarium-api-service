28e459fe6f7c9e16c76f22f7096e4be108ef4833
Max <mxbely98@gmail.com>
2024-12-22 17:54:19 +0200

    add register user and serializer

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from accounts.models import User
+from accounts.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
